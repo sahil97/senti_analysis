956f46165392dc253915d7354d4b4c0cd1c7e166
sahil97 <sahil.sharma071997@gmail.com>
2019-07-26 21:37:27 +0530

    added react routing

--- twtranalysis/src/components/main-wrapper/main-wrapper.js
+++ twtranalysis/src/components/main-wrapper/main-wrapper.js
@@ -1,28 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./main-wrapper.css";
 
-const mainWrapper = () => (
-  <div className="main-wrapper">
-    <div className="twitter-logo">
-      <img className="img-responsive" src="/twtr.png" alt="Twitter-Logo"></img>
-    </div>
-    <div className="wrapper">
-      <div className="text-block">
-        <h1>NLP analysis of Tweets</h1>
-        <p> Built using React, D3, Tensorflow, Flask.</p>
-      </div>
-      <div className="input-wrapper">
-        <input
-          type="text"
-          maxlength="25"
-          id="Hashtag"
-          class="form-control form-control-lg"
-          placeholder="type #Hashtag to search"
-        />
-        <div className="search"></div>
-      </div>
-    </div>
-  </div>
-);
+class MainWrapper extends Component {
+  state = {
+    hashtag: ""
+  };
 
-export default mainWrapper;
+  handleTextChange = event => {
+    this.setState({ hashtag: event.target.value + "1" });
+  };
+
+  render() {
+    return (
+      <div className="main-wrapper">
+        <div className="twitter-logo">
+          <img
+            className="img-responsive"
+            src="/twtr.png"
+            alt="Twitter-Logo"
+          ></img>
+        </div>
+        <div className="wrapper">
+          <div className="text-block">
+            <h1>NLP analysis of Tweets</h1>
+            <p> Built using React, D3, Tensorflow, Flask.</p>
+          </div>
+          <div className="input-wrapper">
+            <input
+              type="text"
+              maxLength="25"
+              id="Hashtag"
+              className="form-control form-control-lg"
+              placeholder="type #Hashtag to search"
+              value={this.state.hashtag}
+              onChange={this.handleTextChange.bind(this)}
+            />
+            <div className="search"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainWrapper;
