441ed575c89eedead8655647c47f45da6a12c22b
sahil97 <sahil.sharma071997@gmail.com>
2019-07-30 17:43:58 +0530

    added line chart using chartjs

--- twtranalysis/src/components/Graphs/line-plot/line-plot.js
+++ twtranalysis/src/components/Graphs/line-plot/line-plot.js
@@ -1,108 +1,105 @@
-import { ResponsiveLine } from "@nivo/line";
-import React from "react";
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+import moment from "moment";
+import pallete from "google-palette";
+class LinePlot extends Component {
+  hoursEarlier = hours => {
+    return moment()
+      .subtract(hours, "h")
+      .toDate();
+  };
 
-const data = [
-  {
-    id: "fake corp. A",
-    data: [
-      { x: "2018-01-01", y: 7 },
-      { x: "2018-01-02", y: 5 },
-      { x: "2018-01-03", y: 11 },
-      { x: "2018-01-04", y: 9 },
-      { x: "2018-01-05", y: 12 },
-      { x: "2018-01-06", y: 16 },
-      { x: "2018-01-07", y: 13 },
-      { x: "2018-01-08", y: 13 }
-    ]
-  },
-  {
-    id: "fake corp. B",
-    data: [
-      { x: "2018-01-04", y: 14 },
-      { x: "2018-01-05", y: 14 },
-      { x: "2018-01-06", y: 15 },
-      { x: "2018-01-07", y: 11 },
-      { x: "2018-01-08", y: 10 },
-      { x: "2018-01-09", y: 12 },
-      { x: "2018-01-10", y: 9 },
-      { x: "2018-01-11", y: 7 }
-    ]
-  }
-];
+  colors = pallete("cb-Dark2", 2).map(hex => {
+    return "#" + hex;
+  });
 
-const MyResponsiveLine = () => (
-  <ResponsiveLine
-    data={data}
-    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-    xScale={{
-      type: "time",
-      format: "%Y-%m-%d",
-      precision: "day"
-    }}
-    xFormat="time:%Y-%m-%d"
-    yScale={{
-      type: "linear",
-      min: "auto",
-      max: "auto"
-    }}
-    axisTop={null}
-    axisRight={null}
-    axisLeft={{
-      legend: "linear scale",
-      legendOffset: 12
-    }}
-    axisBottom={{
-      format: "%b %d",
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      tickValues: "every 2 days",
-      legend: "time scale",
-      legendOffset: -12,
-      legendPosition: "middle"
-    }}
-    colors={{ scheme: "nivo" }}
-    pointSize={10}
-    enablePointLabel={true}
-    pointBorderWidth={1}
-    pointBorderColor={{
-      from: "color",
-      modifiers: [["darker", 0.3]]
-    }}
-    useMesh={true}
-    enableSlices={false}
-    legends={[
+  speedData = {
+    labels: [
+      this.hoursEarlier(10),
+      this.hoursEarlier(9.4),
+      this.hoursEarlier(8),
+      this.hoursEarlier(7),
+      this.hoursEarlier(6),
+      this.hoursEarlier(5),
+      this.hoursEarlier(4)
+    ],
+    datasets: [
       {
-        anchor: "bottom-right",
-        direction: "column",
-        justify: false,
-        translateX: 100,
-        translateY: 0,
-        itemsSpacing: 0,
-        itemDirection: "left-to-right",
-        itemWidth: 80,
-        itemHeight: 20,
-        itemOpacity: 0.75,
-        symbolSize: 12,
-        symbolShape: "circle",
-        symbolBorderColor: "rgba(0, 0, 0, .5)",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemBackground: "rgba(0, 0, 0, .03)",
-              itemOpacity: 1
+        label: "Positive",
+        data: [0, 59, 75, 20, 20, 55, 40],
+        lineTension: 0.1,
+        backgroundColor: "transparent",
+        borderColor: this.colors[0],
+        pointRadius: 5,
+        pointHoverRadius: 10,
+        pointHitRadius: 30,
+        pointBorderWidth: 2,
+        pointStyle: "rectRounded"
+      },
+      {
+        label: "Negative",
+        data: [1, 29, 73, 10, 50, 25, 10],
+        lineTension: 0.1,
+        backgroundColor: "transparent",
+        borderColor: this.colors[1],
+        pointRadius: 5,
+        pointHoverRadius: 10,
+        pointHitRadius: 30,
+        pointBorderWidth: 2,
+        pointStyle: "rectRounded"
+      }
+    ]
+  };
+
+  chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      display: true,
+      position: "top",
+      labels: {
+        boxWidth: 80,
+        fontColor: "black"
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          type: "time",
+          time: {
+            unit: "hour",
+            unitStepSize: 1.5,
+            round: "hour",
+            tooltipFormat: "h:mm:ss a",
+            displayFormats: {
+              hour: "h:mm A"
             }
           }
-        ]
-      }
-    ]}
-  />
-);
+        }
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            color: "black",
+            borderDash: [2, 5]
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "Tweet Count",
+            fontColor: "white"
+          }
+        }
+      ]
+    }
+  };
 
-export default MyResponsiveLine;
+  render() {
+    return (
+      <div>
+        <Line data={this.speedData} options={this.chartOptions} height={400} />
+      </div>
+    );
+  }
+}
+
+export default LinePlot;
