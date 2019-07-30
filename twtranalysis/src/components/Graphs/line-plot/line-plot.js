ba174d7ed9de292bd28de28898711eb83c3299fe
sahil97 <sahil.sharma071997@gmail.com>
2019-07-30 20:54:34 +0530

    added working line plot

--- twtranalysis/src/components/Graphs/line-plot/line-plot.js
+++ twtranalysis/src/components/Graphs/line-plot/line-plot.js
@@ -1,56 +1,11 @@
 import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
-import moment from "moment";
 import pallete from "google-palette";
-class LinePlot extends Component {
-  hoursEarlier = hours => {
-    return moment()
-      .subtract(hours, "h")
-      .toDate();
-  };
 
+class LinePlot extends Component {
   colors = pallete("cb-Dark2", 2).map(hex => {
     return "#" + hex;
   });
-
-  speedData = {
-    labels: [
-      this.hoursEarlier(10),
-      this.hoursEarlier(9.4),
-      this.hoursEarlier(8),
-      this.hoursEarlier(7),
-      this.hoursEarlier(6),
-      this.hoursEarlier(5),
-      this.hoursEarlier(4)
-    ],
-    datasets: [
-      {
-        label: "Positive",
-        data: [0, 59, 75, 20, 20, 55, 40],
-        lineTension: 0.1,
-        backgroundColor: "transparent",
-        borderColor: this.colors[0],
-        pointRadius: 5,
-        pointHoverRadius: 10,
-        pointHitRadius: 30,
-        pointBorderWidth: 2,
-        pointStyle: "rectRounded"
-      },
-      {
-        label: "Negative",
-        data: [1, 29, 73, 10, 50, 25, 10],
-        lineTension: 0.1,
-        backgroundColor: "transparent",
-        borderColor: this.colors[1],
-        pointRadius: 5,
-        pointHoverRadius: 10,
-        pointHitRadius: 30,
-        pointBorderWidth: 2,
-        pointStyle: "rectRounded"
-      }
-    ]
-  };
-
   chartOptions = {
     responsive: true,
     maintainAspectRatio: false,
@@ -58,7 +13,7 @@ class LinePlot extends Component {
       display: true,
       position: "top",
       labels: {
-        boxWidth: 80,
+        boxWidth: 50,
         fontColor: "black"
       }
     },
@@ -67,13 +22,11 @@ class LinePlot extends Component {
         {
           type: "time",
           time: {
-            unit: "hour",
-            unitStepSize: 1.5,
-            round: "hour",
-            tooltipFormat: "h:mm:ss a",
-            displayFormats: {
-              hour: "h:mm A"
-            }
+            unit: "hours",
+            unitStepSize: 0.25,
+            round: "minutes",
+            tooltipFormat: "h:mm a",
+            displayFormats: { hours: "h:mm a" }
           }
         }
       ],
@@ -94,11 +47,46 @@ class LinePlot extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <Line data={this.speedData} options={this.chartOptions} height={400} />
-      </div>
-    );
+    let speedData = {
+      labels: this.props.linePlotData.labels,
+      datasets: [
+        {
+          label: "Total Tweets",
+          data: this.props.linePlotData.plotData,
+          lineTension: 0.1,
+          backgroundColor: "transparent",
+          borderColor: "white",
+          borderWidth: 3,
+          pointRadius: 5,
+          pointHoverRadius: 10,
+          pointHitRadius: 30,
+          pointBorderWidth: 2,
+          pointStyle: "rectRounded"
+        }
+        // {
+        //   label: "Negative",
+        //   data: [1, 29, 73, 10, 50, 25, 10],
+        //   lineTension: 0.1,
+        //   backgroundColor: "transparent",
+        //   borderColor: this.colors[1],
+        //   pointRadius: 5,
+        //   pointHoverRadius: 10,
+        //   pointHitRadius: 30,
+        //   pointBorderWidth: 2,
+        //   pointStyle: "rectRounded"
+        // }
+      ]
+    };
+
+    let LinePlot = <h1>Loading</h1>;
+
+    if (this.props.linePlotData.labels) {
+      LinePlot = (
+        <Line data={speedData} options={this.chartOptions} height={400} />
+      );
+    }
+
+    return <div>{LinePlot}</div>;
   }
 }
 
--- twtranalysis/src/components/results-page/results-page.js
+++ twtranalysis/src/components/results-page/results-page.js
@@ -3,13 +3,32 @@ import "./results-page.css";
 import Nav from "../Navbar/Nav";
 import MyResponsiveLine from "../Graphs/line-plot/line-plot";
 import Aux from "../../HOC/Aux/Aux";
+import axios from "../../axios";
+import moment from "moment";
 
 class ResultsPage extends Component {
   state = {
     hashtag: "",
     retweets: 0,
-    tweet_count: {},
-    total_count: 0
+    total_count: 0,
+    linePlotData: {}
+  };
+
+  response = {
+    data: {
+      retweets: 586,
+      time_count: [
+        { count: 189, time: "Tue, 30 Jul 2019 11:15:00 GMT" },
+        { count: 171, time: "Tue, 30 Jul 2019 11:45:00 GMT" },
+        { count: 170, time: "Tue, 30 Jul 2019 12:00:00 GMT" },
+        { count: 166, time: "Tue, 30 Jul 2019 12:15:00 GMT" },
+        { count: 154, time: "Tue, 30 Jul 2019 11:30:00 GMT" },
+        { count: 103, time: "Tue, 30 Jul 2019 11:00:00 GMT" },
+        { count: 33, time: "Tue, 30 Jul 2019 12:30:00 GMT" }
+      ],
+      length: 7,
+      total_count: 986
+    }
   };
 
   componentDidMount() {
@@ -17,14 +36,51 @@ class ResultsPage extends Component {
     for (let param of query.entries()) {
       this.setState({ hashtag: param[1] });
     }
+
+    // axios
+    //   .post("", {
+    //     hashtag: "India"
+    //   })
+    //   .then(res => console.log("res", res));
+
+    setTimeout(() => {
+      this.showTweets(this.response);
+    }, 3000);
   }
+
+  showTweets = response => {
+    let labels = [];
+    let plotData = [];
+
+    response.data.time_count = response.data.time_count.sort((a, b) => {
+      a = moment(a.time, "ddd, DD MMM YYYY HH:mm:ss").toDate();
+      b = moment(b.time, "ddd, DD MMM YYYY HH:mm:ss").toDate();
+      return a < b ? -1 : a > b ? 1 : 0;
+    });
+
+    response.data.time_count.forEach(tweet => {
+      labels.push(moment(tweet.time).toDate());
+      plotData.push(tweet.count);
+    });
+
+    this.setState({
+      total_count: response.total_count,
+      retweets: response.retweets,
+      linePlotData: {
+        labels: labels,
+        plotData: plotData
+      }
+    });
+
+    console.log("Done");
+  };
 
   render() {
     return (
       <Aux>
         <Nav hashtag={this.state.hashtag} />
         <div className="linePlotContainer">
-          <MyResponsiveLine />
+          <MyResponsiveLine linePlotData={this.state.linePlotData} />
         </div>
       </Aux>
     );
